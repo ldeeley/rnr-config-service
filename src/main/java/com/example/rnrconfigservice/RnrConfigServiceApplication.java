910471defc891067999557f393fc196878d7c11c
ldeeley <lester.deeley@yahoo.com>
2023-06-30 17:22:47 +0100

    Initial commit

---
+++ src/main/java/com/example/rnrconfigservice/RnrConfigServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.rnrconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnrConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnrConfigServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rnrconfigservice/RnrConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rnrconfigservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnrConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
